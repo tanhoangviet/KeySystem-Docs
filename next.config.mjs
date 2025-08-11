491965ccc3f81a4f93aad07b101353efe2822fa6
Footagesus <alisher.bahitbek.discord2@gmail.com>
2025-08-11 19:43:53 +0500

    Foo update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repo = 'WindUI-Docs';
 
 /** @type {import('next').NextConfig} */
 const config = {
+    output: 'export',
+    basePath: isGithubPages ? `/${repo}` : '',
+    assetPrefix: isGithubPages ? `/${repo}/` : '',
     reactStrictMode: true,
-    experimental: {
-        optimizeCss: false
-    }
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default withMDX(config);
