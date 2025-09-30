f1e838a45b4e50f7fbbbdbdc0575b22438058765
Footagesus <alisher.bahitbek.discord2@gmail.com>
2025-09-30 21:11:38 +0500

    Foo added title logo description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
-import '@/app/global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import '@/app/global.css'
+import { RootProvider } from 'fumadocs-ui/provider'
+import { Inter } from 'next/font/google'
+import type { ReactNode } from 'react'
 
-const inter = Inter({
-  subsets: ['latin'],
-});
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'WindUI Documentation',
+  description: 'Official Documentation for the Roblox WindUI UI Library',
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider 
-            search={{
-                enabled: false,
-            }}
-        >
-            {children}
+        <RootProvider search={{ enabled: false }}>
+          {children}
         </RootProvider>
       </body>
     </html>
-  );
+  )
 }
